659f757eec969e3aac54add1f402206f3b5029ae
mohamed <elkahzen1@gmail.com>
2021-09-03 12:11:28 -0500

    added studios

---
+++ Studios/AreaOfCircle/AreaOfCircle/AreaOfCircle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfCircle
+{
+    class AreaOfCircle
+    {
+        public static String Area()
+        {
+            Console.WriteLine("enter a radious: ");
+            String input = Console.ReadLine();
+            while (int.Parse(input) <= 0 )
+            {
+                Console.WriteLine("you need a positive number, enter a radious: ");
+                input = Console.ReadLine();
+            }
+
+            Double pi = Math.PI;
+            Double areaOfCircle =  pi * int.Parse(input) * int.Parse(input);
+            String output = "The area of the circle deppanding on the radious is: " + areaOfCircle;
+            Console.WriteLine(output);
+
+            return output;
+
+        }
+    }
+}
